a1864c77265abe8ff0451b60a496340383cc0a6e
Tyler Wade <vaxonreal@gmail.com>
2025-03-03 11:56:48 -0500

    Added jump to top button.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Hero from "@/components/home/hero";
 import Expertise from "@/components/home/expertise";
 import WhatMakesMeDifferent from "@/components/home/what-makes-me-different";
@@ -5,8 +6,37 @@ import Projects from "@/components/home/projects";
 import Footer from "@/components/footer";
 import Contact from "@/components/home/contact";
 import Education from "@/components/home/education";
+import {useEffect, useState} from "react";
+import {IconArrowNarrowUp} from "@tabler/icons-react";
 
 export default function Home() {
+
+  const [atTopPage, setAtTopPage] = useState(true);
+
+  function handleAtTopPage() {
+
+    if (window.scrollY < 600) {
+      setAtTopPage(true);
+    }
+    else {
+      setAtTopPage(false);
+    }
+  }
+
+  function scrollToTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
+
+  useEffect(() => {
+
+    window.addEventListener('scroll', handleAtTopPage);
+
+    return () => {
+      window.removeEventListener('scroll', handleAtTopPage);
+    }
+
+  }, []);
+
   return (
     <div className="w-full min-h-screen flex flex-col items-center">
       <Hero />
@@ -16,6 +46,15 @@ export default function Home() {
       <Education />
       <Contact />
       <Footer />
+
+      {!atTopPage && (
+        <button
+          className="bg-accent hover:bg-accentDark text-white p-2 fixed z-30 bottom-4 right-4 duration-300 active:bg-white active:text-accent shadow-lg"
+          onClick={scrollToTop}
+        >
+          <IconArrowNarrowUp className="" />
+        </button>
+      )}
     </div>
   );
 }
